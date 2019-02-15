1de003e083bb45397ff883081bd0e204d0843a51
657524896 <to1ife>
2019-02-16 03:21:21 +0800

    udp公网端口暂设8001

--- luaService/udpSocket.lua
+++ luaService/udpSocket.lua
@@ -6,17 +6,7 @@ local function server()
 	host = socket.udp(function(str, from)
 		print("server recv", str, socket.udp_address(from))
 		socket.sendto(host, from, "OK " .. str)
-	end , "127.0.0.1", 8765)	-- bind an address
-end
-
-local function client()
-	local c = socket.udp(function(str, from)
-		print("client recv", str, socket.udp_address(from))
-	end)
-	socket.udp_connect(c, "127.0.0.1", 8765)
-	for i=1,20 do
-		socket.write(c, "hello " .. i)	-- write to the address by udp_connect binding
-	end
+	end , "0.0.0.0", 8001)	-- bind an address
 end
 
 skynet.start(function()
